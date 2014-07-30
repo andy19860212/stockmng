26d2163bb6a0df8b09e3c57dbc8c2ede54b4268a
andy19860212 <andytrans19860212@gmail.com>
2014-07-30 15:37:17 +0800

    完成股票系统基本功能

---
+++ src/com/andy/service/NetAssetStockPriceHelper.scala
@@ -0,0 +1,44 @@
+package com.andy.service
+
+import com.andy.model.url.StockPriceFinder
+
+import scala.actors.{TIMEOUT, Actor}
+
+/**
+ * Created by wb-zhangwei01 on 2014/7/30.
+ * 更新股票价格的actor
+ */
+object NetAssetStockPriceHelper {
+  val symbolsAndUntis = StockPriceFinder.getTickersAndUnits
+
+  def getInitialTableValues: Array[Array[Any]] = {
+    val emptyArrayOfArrayOfAny = new Array[Array[Any]](0, 0)
+    (emptyArrayOfArrayOfAny /: symbolsAndUntis) { (data, element) =>
+      val (ticker, units) = element
+      data ++ Array(List(ticker, units, "?", "?").toArray)
+    }
+  }
+
+  def fetchPrice(updater: Actor) = actor {
+    val caller = self
+    symbolsAndUntis.keys.foreach { ticker =>
+      actor {
+        caller !(tciker, StockPriceFinder.getLatestClosingPrice(ticker))
+      }
+    }
+
+
+    val netWorth = (0.0 /: (1 to symbolsAndUntis.size)) { (worth, index) =>
+      receiveWithin(10000) {
+        case (ticker: String, latestClosingPrice: Double) =>
+          val units = symbolsAndUntis(ticker)
+          val value = units * latestClosingPrice
+          updater !(ticker, units, latestClosingPrice, value)
+          worth + value
+        case TIMEOUT =>
+          worth
+      }
+    }
+    updater ! netWorth
+  }
+}
---
+++ src/com/andy/view/NetAssetAppGUI.scala
@@ -0,0 +1,89 @@
+package com.andy.view
+
+import java.awt.Color
+import java.util.Date
+
+import com.andy.service.NetAssetStockPriceHelper
+
+import scala.actors._
+import Actor._
+import scala.actors.scheduler.SingleThreadedScheduler
+import scala.swing._
+import event._
+
+/**
+ * Created by wb-zhangwei01 on 2014/7/30.
+ */
+object NetAssetAppGUI extends SimpleSwingApplication {
+  def top = mianFrame
+
+  val mainFrame = new MainFrame {
+    title = "Net Asset"
+    val dateLable = new Label {
+      text = "最近更新时间:--------------"
+    }
+    val valuesTable = new Table(
+      NetAssetStockPriceHelper.getInitialTableValues,
+      Array("股票名称", "持股数量", "最新单股价", "您的所得")) {
+      showGrid = true
+      gridColor = Color.BLACK
+    }
+
+    val updateButton = new Button {
+      text = "更新股票信息"
+    }
+    val netAssetLabel = new Label {
+      text = "总价格:????"
+    }
+
+    contents += new BoxPanel(Orientation.Vertical) {
+      contents += dateLable
+      contents += valuesTable
+      contents += new ScrollPane(valuesTable)
+
+      contents += new FlowPanel {
+        contents += updateButton
+        contents += netAssetLabel
+      }
+    }
+
+    listenTo(updateButton)
+
+
+
+    reactions += {
+      case ButtonClicked(button) =>
+        button.enabled = false
+        NetAssetStockPriceHelper fetchPrice uiUpdater
+    }
+
+    val uiUpdater = new Actor {
+      def act = {
+        loop {
+          react {
+            case (ticker: String, units: Int, price: Double, value: Double) => updateTable(ticker, units, price, value)
+            case netAsset =>
+              netAssetLabel.text = "总价格:" + netAsset
+              dateLable.text = "最近更新时间:" + new Date()
+              updateButton.enabled = true
+          }
+        }
+      }
+
+      override protected def scheduler() = new SingleThreadedScheduler
+    }
+
+    uiUpdater.start()
+
+    def updateTable(ticker: String, units: Int, price: Double, value: Double) {
+      for (i <- 0 until valuesTable.rowCount) {
+        if (valuesTable(i, 0) == ticker) {
+          valuesTable(i, 2) = price
+          valuesTable(i, 3) = value
+        }
+      }
+    }
+  }
+
+
+}
