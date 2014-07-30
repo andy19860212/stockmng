55ffec275561d6f46ee2ca29781624acce2caaa0
andy19860212 <andytrans19860212@gmail.com>
2014-07-30 13:13:35 +0800

    为获取股票价格添加并发测试

---
+++ src/com/andy/service/FinderTotalWorthConcurrent.scala
@@ -0,0 +1,47 @@
+package com.andy.service
+
+import java.util.Date
+
+import com.andy.model.url.StockPriceFinder
+import scala.actors._
+import Actor._
+
+/**
+ * Created by wb-zhangwei01 on 2014/7/30.
+ * 添加并发控制
+ * 消耗时间约####Took 10.736967 seconds####
+ */
+object FinderTotalWorthConcurrent extends App {
+
+  val symbolsAndUnits = StockPriceFinder.getTickersAndUnits
+
+  val caller = self
+  println("Today is " + new Date())
+  println("Ticker Units Closing Price($) Total Value($)")
+
+  val startTime = System.nanoTime()
+
+  symbolsAndUnits.keys.foreach {
+    ticker =>
+      actor {
+        caller !(ticker, StockPriceFinder.getLatestClosingPrice(ticker))
+      }
+  }
+
+  val networth = (0.0 /: (1 to symbolsAndUnits.size)) { (worth, index) =>
+    receiveWithin(10000) {
+      case (ticker: String, latestClosingPrice: Double) =>
+        val units = symbolsAndUnits(ticker)
+        val value = units * latestClosingPrice
+        println("%-7s %-5d %-16f %f".format(ticker, units, latestClosingPrice, value))
+        worth + value
+      case TIMEOUT =>
+        worth
+    }
+  }
+
+  val endTime = System.nanoTime()
+  //println("The total value of your inverstments is $ " + netWorth)
+  println("Took %f seconds".format((endTime - startTime) / 1000000000.0))
+
+}
